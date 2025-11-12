91659ccc5ff0c3f0b19236eced30c7a7dd3b6331
Kera <95004283+itzkera@users.noreply.github.com>
2025-11-11 21:37:03 -0500

    Update index.js

--- discord bot/index.js
+++ discord bot/index.js
@@ -1,6 +1,6 @@
 const { Client, GatewayIntentBits, Events } = require('discord.js');
 
-// Create a new client instance
+
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
@@ -9,23 +9,20 @@ const client = new Client({
     ],
 });
 
-// When the bot is ready
+
 client.once(Events.ClientReady, () => {
     console.log(`Logged in as ${client.user.tag}!`);
-    // Set the bot's status
     client.user.setActivity('yap', { type: 'PLAYING' });
 });
 
-// When the bot receives a message
+
 client.on(Events.MessageCreate, message => {
-    // Ignore messages from the bot itself
     if (message.author.bot) return;
 
-    // Simple command: !ping
     if (message.content === '!up') {
         message.reply('bot,backend is up!');
     }
 });
 
-// Replace 'YOUR_BOT_TOKEN' with your actual bot token
+
 client.login('YOUR_BOT_TOKEN');
