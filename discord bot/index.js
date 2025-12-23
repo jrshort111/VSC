bd17019e2d8a18c9617aa9e80f79575031fccf90
Kera <95004283+itzkera@users.noreply.github.com>
2025-12-23 11:16:34 -0500

    env time

--- discord bot/index.js
+++ discord bot/index.js
@@ -16,3 +16,15 @@ const client = new Client({
 client.once('ready', () => {
     console.log("[BOT] Logged In as ${client.user.tag]");
 });
+
+
+client.on('messageCreate', message => {
+    if (message.author.bot) return;
+
+          if (messsage.content.toLowerCase() === 'ping') {
+              message.reply('Latency is ${Date.now() - message.createdTimestamp}ms');
+          }
+    ]);
+
+client.login(process.env.TOKEN);
+
